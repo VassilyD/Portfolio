1309e222a24c3e6711f2d29dd504b546b5d3ae74
VassilyD <vassily.dubois@gmail.com>
2017-12-01 12:33:06 +0100

    def const texte navbarpour rand js

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+const listeLettres = ['À', 'a', 'b', 'c', 'e', '+', 'p', '$',
+					  'P', 'o', 'r', '!', 'E', 's', '=', 'i',
+					  'y', '#', 'm', 'j', 'g', 'é', '&', 'l',
+					  'S', 'x', 'u', 'R', '?', '@', '1', '2',
+					  '3', '4', '5', '6', '7', '8', '9', '0',
+					  't', 'n', 'è', '%', '£', '*', ' ', 'w'];
+
+const aProposTxt = 'À propos';
+const projetsTxt = 'Projets';
+const expeTxt = 'Expérimental';
+const socialTxt = 'Réseaux sociaux';
